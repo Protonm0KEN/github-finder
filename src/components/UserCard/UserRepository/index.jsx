e12897f1b5d52b8899a210a6da5058fdc30902f3
macbook <gamerovichakbar@gmail.com>
2023-02-15 00:12:34 +0500

    project

--- src/components/UserCard/UserRepository/index.jsx
+++ src/components/UserCard/UserRepository/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import style from "../UserCard.module.scss";
 
-const UserRepository = ({date, repository}) => {
-    const {stargazers_count, name, svn_url} = repository;
+const UserRepository = ({repository}) => {
+    const {stargazers_count, name, svn_url, pushed_at} = repository;
     return (
         <div className={style["user__sorting-repository"]}>
             <div className={style["user__sorting-repository-left"]}>
                 <p className={style["repository-title"]}>{name}</p>
                 <p className={style["repository-starsCount"]}>Количество звезд: {stargazers_count}</p>
-                <p className={style["repository-dateOfCreation"]}>Дата добавления: {date}</p>
+                <p className={style["repository-dateOfCreation"]}>Дата добавления: {pushed_at.substring(0, 10)}</p>
             </div>
             <div className={style["user__sorting-repository-right"]}>
                 <a
--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
 import style from "./UserCard.module.scss";
 import UserRepository from "./UserRepository";
 
 
 const UserCard = ({dataOfUser, Repositories}) => {
+
     const date = dataOfUser.created_at.substring(0, 10);
 
+    const [sortByStars, setSortByStars ] = useState(false);
+    const [sortByDate, setSortByDate ] = useState(false);
+    const [sortByName, setSortByName ] = useState(false);
 
+
+
+    function compareNumeric(a, b) {
+        if (a > b) {
+            return -1;
+        }
+        if (a === b) return 0;
+        if (a < b) {
+            return 1;
+        }
+    }
+    const sortByDateHandle = () => {
+        setSortByStars(false);
+        setSortByDate(true);
+        setSortByName(false);
+    };
+    const sortByNameHandle = () => {
+        setSortByStars(false);
+        setSortByDate(false);
+        setSortByName(true);
+    };
+    const sortByStarsHandle = () => {
+        setSortByStars(true);
+        setSortByDate(false);
+        setSortByName(false);
+    };
     return (
         <div>
             <div className={style["user"]}>
@@ -32,20 +62,47 @@ const UserCard = ({dataOfUser, Repositories}) => {
                 <div className={style["user__sorting"]}>
                     <h2 className={style["user__sorting-title"]}>Сортировка</h2>
                     <div className={style["user__sorting-buttons"]}>
-                        <button className={`${style["user__sorting-button"]}`}>Имя</button>
-                        <button className={style["user__sorting-button"]}>Звезды</button>
-                        <button className={style["user__sorting-button"]}>Дата</button>
+                        <button onClick={sortByNameHandle} className={`${style["user__sorting-button"]}`}>Имя</button>
+                        <button onClick={sortByStarsHandle} className={style["user__sorting-button"]}>Звезды</button>
+                        <button onClick={sortByDateHandle} className={style["user__sorting-button"]}>Дата</button>
                     </div>
                     <div className={style["user__sorting-repositores"]}>
-                        {Repositories.map((repository, index) => {
-                            return(
-                                <UserRepository key = {index}
-                                    repository = {repository}
-                                    date = {date}>
-
-                                </UserRepository>
-                            );
-                        })}
+                        { sortByName ?
+                            Repositories
+                                .sort((a, b) => {
+                                    return a.name.localeCompare(b.name);
+                                })
+                                .map((repository, index) => {
+                                    return (
+                                        <UserRepository
+                                            key={index}
+                                            repository={repository}
+                                            date={date}>
+                                        </UserRepository>
+                                    );
+                                }) :
+                            sortByStars ?
+                                Repositories
+                                    .sort((a, b) => compareNumeric(a.stargazers_count, b.stargazers_count))
+                                    .map((repository, index) => {
+                                        return (
+                                            <UserRepository
+                                                key={index}
+                                                repository={repository}
+                                                date={date}>
+                                            </UserRepository>
+                                        );
+                                    })
+                                : sortByDate ?
+                                    Repositories.sort( (a, b) => compareNumeric(a.created_at, b.created_at)).map((repository, index) => {
+                                        return (
+                                            <UserRepository
+                                                key={index}
+                                                repository={repository}
+                                                date={date}>
+                                            </UserRepository>
+                                        );
+                                    }):  null}
                     </div>
                 </div>
             </div>
